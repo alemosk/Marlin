d499c1cfa01b2ee7e9aff2514f2310a84004587e
Aleksei Baranov <aleksei.ee@gmail.com>
2020-12-25 22:26:28 +0200

    Changed Hybrid Threshold settings.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2509,16 +2509,16 @@
    */
   #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     100  // [mm/s]
-  #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     100
-  #define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD     100
-  #define Z2_HYBRID_THRESHOLD      3
+  #define X_HYBRID_THRESHOLD     255  // [mm/s]
+  #define X2_HYBRID_THRESHOLD    255
+  #define Y_HYBRID_THRESHOLD     255
+  #define Y2_HYBRID_THRESHOLD    255
+  #define Z_HYBRID_THRESHOLD     255
+  #define Z2_HYBRID_THRESHOLD    255
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
-  #define E0_HYBRID_THRESHOLD    100
-  #define E1_HYBRID_THRESHOLD     30
+  #define E0_HYBRID_THRESHOLD    255
+  #define E1_HYBRID_THRESHOLD     255
   #define E2_HYBRID_THRESHOLD     30
   #define E3_HYBRID_THRESHOLD     30
   #define E4_HYBRID_THRESHOLD     30
